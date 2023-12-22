c4591a610079b45c1b93df40279960781e706eef
Colin <colinjswee@gmail.com>
2023-12-22 18:50:43 -0500

    Add missing textures for custom armors

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Fun Treasure mod for Better than Adventure
 
 A mod for the beta minecraft mod/fork "Better than Adventure" that adds some fun treasure items to be found across the world. 
+
+## CREDITS <3
+
+Idea & textures for piston boots from adventure backpack mod
+https://github.com/chiqors/AdventureBackpack-MCJava
+
+Kefke on bta discord, I stole the Idea for silver sword from you, sorry it's the worst item in the mod
+
+Everybody who helped me answer stupid questions in bta modding discord
--- src/main/java/csweetla/fun_treasure/FunTreasure.java
+++ src/main/java/csweetla/fun_treasure/FunTreasure.java
@@ -8,7 +8,7 @@ import net.minecraft.core.item.ItemFood;
 import net.minecraft.core.item.material.ToolMaterial;
 import net.minecraft.core.item.tag.ItemTags;
 import net.minecraft.core.item.tool.ItemToolSword;
-import turniplabs.halplibe.helper.ItemHelper;
+import turniplabs.halplibe.helper.*;
 
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemArmor;
@@ -16,14 +16,13 @@ import net.minecraft.core.item.material.ArmorMaterial;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.RecipeHelper;
-import turniplabs.halplibe.helper.SoundHelper;
-import turniplabs.halplibe.helper.TextureHelper;
 
 public class FunTreasure implements ModInitializer {
     public static final String MOD_ID = "fun_treasure";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static ArmorMaterial armorMaterialPistonBoots;
+	public static ArmorMaterial armorMaterialDiving;
 	public static Item armorItemPistonBoots;
 	public static Item armorItemDivingHelmet;
 	public static Item toolItemSilverSword;
@@ -36,18 +35,24 @@ public class FunTreasure implements ModInitializer {
 	public static ItemFood FoodItemBananas;
 	public static ItemFood FoodItemFruitSalad;
 
-	@Override
-    public void onInitialize() {
+	public void initializeSounds() {
+		SoundHelper.addSound(MOD_ID,"rope_whoosh.ogg");
+	}
+
+	public void initializeArmorMaterials() {
+		armorMaterialPistonBoots = ArmorHelper.createArmorMaterial("piston_boots",220,50.0F,50.0F,20.0F,120.0F);
+		armorMaterialDiving = ArmorHelper.createArmorMaterial("diving",220,20.0F,60.0F,20.0F,20.0F);
+	}
+
+	public void initializeItems() {
 		int[] tex_coords;
 		int curr_id = ItemHelper.findOpenIds(10);
-		LOGGER.info(MOD_ID + " initialized.");
 
-		SoundHelper.addSound(MOD_ID,"rope_whoosh.ogg");
-
-		armorItemPistonBoots = new ItemArmor(MOD_ID + ".piston_boots", curr_id++, ArmorMaterial.iron, 3).setIconCoord(2, 3);
+		tex_coords = TextureHelper.getOrCreateItemTexture(MOD_ID,"piston_boots.png");
+		armorItemPistonBoots = new ItemArmor(MOD_ID + ".piston_boots", curr_id++, armorMaterialPistonBoots, 3).setIconCoord(tex_coords[0],tex_coords[1]);
 
 		tex_coords = TextureHelper.getOrCreateItemTexture(MOD_ID,"diving_helmet.png");
-		armorItemDivingHelmet = new ItemArmor(MOD_ID + ".diving_helmet", curr_id++, ArmorMaterial.iron, 0).setIconCoord(tex_coords[0],tex_coords[1]);
+		armorItemDivingHelmet = new ItemArmor(MOD_ID + ".diving_helmet", curr_id++, armorMaterialDiving, 0).setIconCoord(tex_coords[0],tex_coords[1]);
 
 		tex_coords = TextureHelper.getOrCreateItemTexture(MOD_ID,"silver_sword.png");
 		//noinspection unchecked
@@ -75,17 +80,29 @@ public class FunTreasure implements ModInitializer {
 
 		tex_coords = TextureHelper.getOrCreateItemTexture(MOD_ID,"fruit_salad.png");
 		FoodItemFruitSalad = (ItemFood) new ItemFood(MOD_ID + ".fruit_salad",curr_id++,20,false).setIconCoord(tex_coords[0],tex_coords[1]);
+	}
 
+	private void initializeRecipes() {
 		RecipeHelper.Crafting.createShapelessRecipe(FoodItemFruitSalad,1,
-			 new Object[]{
-				 	Item.bowl,
-                    Item.foodApple,
-				 	Item.cherry,
-                    FoodItemBananas,
-                    FoodItemGrapes,
-                    FoodItemOrange
-            }
-        );
+			new Object[]{
+				Item.bowl,
+				Item.foodApple,
+				Item.cherry,
+				FoodItemBananas,
+				FoodItemGrapes,
+				FoodItemOrange
+			}
+		);
+	}
+
+	@Override
+    public void onInitialize() {
+		LOGGER.info(MOD_ID + " initialized.");
+
+		initializeSounds();
+		initializeArmorMaterials();
+		initializeItems();
+		initializeRecipes();
 
 	}
 }
