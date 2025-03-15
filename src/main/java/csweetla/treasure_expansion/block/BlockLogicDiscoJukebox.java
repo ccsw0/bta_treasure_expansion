6ed1c62455213c67fb9d613ab6d6d0446dbfbbf4
Colin <colinjswee@gmail.com>
2025-03-15 19:19:42 -0400

    Cleaned up code some + improved disco jukeboxes

--- src/main/java/csweetla/treasure_expansion/block/BlockLogicDiscoJukebox.java
+++ src/main/java/csweetla/treasure_expansion/block/BlockLogicDiscoJukebox.java
@@ -4,8 +4,12 @@ import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogicJukebox;
 import net.minecraft.core.block.Blocks;
 import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.block.entity.TileEntityActivator;
+import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.util.helper.Direction;
+import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,6 +19,22 @@ public class BlockLogicDiscoJukebox extends BlockLogicJukebox {
 		super(block);
 		block.withEntity(TileEntityJukeboxDisco::new);
 	}
+
+	@Override
+	public boolean onBlockRightClicked(World world, int x, int y, int z, Player player, Side side, double xPlaced, double yPlaced) {
+		TileEntity te = world.getTileEntity(x,y,z);
+		if (te instanceof TileEntityJukeboxDisco) {
+			TileEntityJukeboxDisco jukeboxEnt =  (TileEntityJukeboxDisco) te;
+			jukeboxEnt.displayTitle();
+		}
+
+		return true;
+	}
+
+	@Override
+	public void onActivatorInteract(World world, int x, int y, int z, TileEntityActivator activator, Direction direction) {
+	}
+
 
 	@Nullable
 	@Override
